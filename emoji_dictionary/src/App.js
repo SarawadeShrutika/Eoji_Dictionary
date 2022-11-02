d9a351f1d1daa4a8c24e5e6428c2b04561ec65bd
SarawadeShrutika <113629567+SarawadeShrutika@users.noreply.github.com>
2022-11-02 04:33:57 -0700

    Sandbox Code files

---
+++ emoji_dictionary/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emoji_dictionary = {
+  "👹": "Ogre",
+  "👺": "Goblin",
+  "🧛": "Vampire",
+  "👽": "Alien",
+  "🧚": "Fairy",
+  "🧛": "Vampire",
+  "🧜": "Merperson",
+  "🧝": "Elf",
+  "🧞": "Genie",
+  "🧟": "Zombie"
+};
+
+var emojiWeKNow = Object.keys(emoji_dictionary);
+
+export default function App() {
+  const [meaning, setmeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emoji_dictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we don't have this in our database";
+    }
+
+    setmeaning(meaning);
+  }
+  function emojiHAndler(emoji) {
+    var meaning = emoji_dictionary[emoji];
+    setmeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emoji Meaning</h1>
+      <input onChange={emojiInputHandler}></input>
+
+      <h2>{meaning}</h2>
+
+      {emojiWeKNow.map(function (emoji) {
+        return (
+          <span
+            key={emoji}
+            onClick={() => emojiHAndler(emoji)}
+            style={{ cursor: "pointer" }}
+          >
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
